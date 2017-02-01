de75404937a7146129078569226a480149d36f40
Brent Hand <brent@novastel.la>
2017-02-01 12:40:00 -0800

    added updated php file

--- custom-plan-type.php
+++ custom-plan-type.php
@@ -8,33 +8,33 @@
 *
 **/
 
-function register_cpt_plan_type() {
+function register_cpt_translation_type() {
   $labels = array(
-    'name' => _x( 'Plan Types', 'plan_type' ),
-    'singular_name' => _x('Plan Type', 'plan_type'),
-    'add_new' => _x('Add New', 'plan_type'),
-    'add_new_item' => _x( 'Add New Plan Type', 'plan_type'),
-    'edit_item' => _x( 'Edit Plan Type', 'plan_type'),
-    'new_item' => _x( 'New Plan Type', 'plan_type'),
-    'view_items' => _x('View Plan Type', 'plan_tpye'),
-    'search_items' => _x('Search Plan Types', 'plan_types'),
-    'not_found' => _x('No Plan Types Found', 'plan_types'),
-    'not_found_in_trash' => _x('No Plan Types Found In Trash', 'plan_types'),
-    'parent_item_colon' => _x('Parent Plan Type', 'plan_type'),
-    'menu_name' => _x('Plan Types', 'plan_type'),
+    'name' => _x( 'Translation Types', 'translation_type' ),
+    'singular_name' => _x('Translation Type', 'translation_type'),
+    'add_new' => _x('Add New', 'translation_type'),
+    'add_new_item' => _x( 'Add New Translation', ''),
+    'edit_item' => _x( 'Edit Translation', 'translation_type'),
+    'new_item' => _x( 'New Translation', 'translation_type'),
+    'view_items' => _x('View Translation', 'plan_tpye'),
+    'search_items' => _x('Search Translations', 'translation_types'),
+    'not_found' => _x('No Translations Types Found', 'translation_types'),
+    'not_found_in_trash' => _x('No Translation Types Found In Trash', 'translation_types'),
+    'parent_item_colon' => _x('Parent Translation', 'translation_type'),
+    'menu_name' => _x('Translations', 'translation_type'),
   );
 
   $args = array(
     'labels' => $labels,
     'hierarchical' => true,
-    'description' => 'Plan Types',
+    'description' => 'Translation',
     'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes' ),
-    'taxonomies' => array('plans'),
+    'taxonomies' => array('translation'),
     'public' => true,
     'show_ui' => true,
     'show_in_menu' => true,
     'menu_position' => 5,
-    'menu_icon' => 'dashicons-building',
+    'menu_icon' => 'dashicons-translation',
     'show_in_nav_menus' => true,
     'publicly_queryable' => true,
     'exclude_from_serach' => false,
@@ -45,15 +45,15 @@ function register_cpt_plan_type() {
     'capability_type' => 'post',
   );
 
-  register_post_type('plan_type', $args);
+  register_post_type('translation_type', $args);
 }
 
-add_action('init', 'register_cpt_plan_type');
+add_action('init', 'register_cpt_translation_type');
 
-function plans_taxonomy() {
+function translation_taxonomy() {
   register_taxonomy(
-    'plans',
-    'plan_type',
+    'translation',
+    'translation_type',
     array(
       'hierarchical' => true,
       'label' => 'Plans',
